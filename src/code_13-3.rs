30962da1cad3b7b1c04f972a9a5406d15734da8c
Mikihiro Okuwa <relievedFace@gmail.com>
2023-11-23 18:14:33 +0900

    add

---
+++ src/code_13-3.rs
@@ -0,0 +1,53 @@
+use proconio::input;
+use std::collections::VecDeque;
+
+type Graph = Vec<Vec<usize>>;
+
+fn bfs(g: &Graph, s: usize) -> Vec<Option<usize>> {
+    let n = g.len();
+    let mut dist = vec![None; n];
+    let mut que = VecDeque::new();
+
+    // 本に記載されているコード
+    // 始点が0になってる？
+    // dist[0] = Some(0);
+    // que.push_back(0);
+
+    // 始点がsの場合
+    dist[s] = Some(0);
+    que.push_back(s);
+
+    while let Some(v) = que.pop_front() {
+        for x in g[v].iter() {
+            if dist[*x].is_some() {
+                continue;
+            }
+
+            dist[*x] = dist[v].map(|dist_v| dist_v + 1);
+            que.push_back(*x);
+        }
+    }
+
+    dist
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    }
+
+    let mut g = vec![vec![]; n];
+
+    for (a, b) in ab.iter() {
+        g[*a].push(*b);
+        g[*b].push(*a);
+    }
+
+    let dist = bfs(&g, 0);
+
+    for v in 0..n {
+        println!("{}: {:?}", v, dist[v]);
+    }
+}
